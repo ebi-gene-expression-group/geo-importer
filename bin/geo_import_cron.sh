fd953f6a1ff898c28b63d35fdb17c27bd500f1b0
Suhaib Mohammed <suhaib58@gmail.com>
2018-07-27 15:27:10 +0100

    Feature/geo optional parameters (#1)
    * In tis commit mandatory parameters with flags have been implemented to recycyle the same code to cater bulk and single cell and associated file system it requires for imports and exports
    
    * In this commit mandatory parameters with flags have been implemented to recycyle the same code to cater bulk and single cell and associated file system it requires for imports and exports
    
    * typo fixed, moved scripts to bin and exception error message
    
    * bin path added to tests
    
    * fixed typo and print error

---
+++ bin/geo_import_cron.sh
@@ -0,0 +1,110 @@
+#!/usr/bin/env bash
+
+# Source script from the same (prod or test) Atlas environment as this script
+scriptDir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+projectRoot=${scriptDir}/../..
+source $projectRoot/geo_import/geo_import_routines.sh
+source $projectRoot/bash_util/generic_routines.sh
+
+today="`eval date +%Y-%m-%d`"
+
+USAGE="Usage: `basename $0` [-u pgAtlasUser ] [-d dbIdentifier ] [-t bulkORsinglecell ] [-s supportingFilesPath ] [-o output ]"
+    while getopts ":u:d:t:s:o:" params; do
+        case $params in
+            u)
+                pgAtlasUser=$OPTARG;
+                ;;
+            d)
+                dbIdentifier=$OPTARG;
+                ;;
+            t)
+                bulkORsinglecell=$OPTARG;
+                ;;
+            s)
+                supportingFilesPath=$OPTARG;
+                ;;
+            o)
+                outputPath=$OPTARG;
+                ;;
+            ?)
+                echo "Invalid Option Specified"
+                echo "$USAGE" 1>&2 ; exit 1
+                ;;
+        esac
+    done
+
+
+if ! [[ "$bulkORsinglecell" =~ ^(bulk|singlecell)$ ]]; then
+    echo "please provide type -t as 'bulk' or 'singlecell'"
+   exit 1;
+fi
+
+if [ ! -d "$outputPath" ]; then
+    echo " output path $outputPath doesn't exist"
+    exit 1;
+fi
+
+if [ ! -d "$supportingFilesPath" ]; then
+    echo "supporting file $supportingFilesPath path doesn't exist"
+    exit 1;
+fi
+
+# Set up DB connection details
+dbConnection=$(get_db_connection $pgAtlasUser $dbIdentifier)
+if [ $? -ne 0 ]; then
+    echo "ERROR: dbConnection connection not established" >&2    
+    exit 1
+fi 
+
+pushd $supportingFilesPath
+    ## Script which retrieves ENA study ids and organsim names for RNA-seq experiments from ENA
+    ## converts ENA study ids to GEO (GSE) ids using API (eutils.ncbi.nlm.nih.gov/entrez/eutils/)
+    ## ENA ids that do not exist in GEO are recorded in rnaseq_ena_gse_pooling.log
+    ## Filters GSE ids that have already been loaded in AE2
+    ## output of the script list of filtered GSE_ids/ENA_study_id as geo_${bulkORsinglecell}_rnaseq.tsv in desired output path
+    $projectRoot/geo_import/rnaseq_ena_gse_pooling.py --type $bulkORsinglecell --output $supportingFilesPath > ${bulkORsinglecell}_ena_gse_pooling.$today.log
+    if [ $? -ne 0 ]; then
+        echo "ERROR: ${bulkORsinglecell}_ena_gse_pooling" >&2    
+        exit 1
+    fi 
+
+    ## ENA_study_ids list that do no exist in GEO import that will help curators to fetch studies from ENA directly
+    makeNotInGEOList() {
+        gsePoolingLog=$1
+        type=$(echo $gsePoolingLog | awk -F'_' '{print $1}')
+        cat $gsePoolingLog | grep "Not in GEO" | awk -F" " '{print $5}' > ${bulkORsinglecell}_ENA_IDs_NotInGEO.tmp
+        test -s "$gsePoolingLog" || (  >&2 echo "$0 gse pooling log file not found: $gsePoolingLog" ; exit 1 )
+        if [ "$type" == "bulk" ]; then
+            join -1 1 -2 2 -o 1.1,2.1 <(cat ${bulkORsinglecell}_ENA_IDs_NotInGEO.tmp | sort ) <(curl -s 'https://www.ebi.ac.uk/fg/rnaseq/api/tsv/getBulkRNASeqStudiesInSRA' | tail -n +2 | sort -k 2) | sort -u
+        elif [ "$type" == "singlecell" ]; then
+            join -1 1 -2 2 -o 1.1,2.1 <(cat ${bulkORsinglecell}_ENA_IDs_NotInGEO.tmp | sort ) <(curl -s 'https://www.ebi.ac.uk/fg/rnaseq/api/tsv/getSingleCellStudies' | tail -n +2 | sort -k 2) | sort -u
+        fi
+        rm -rf ${bulkORsinglecell}_ENA_IDs_NotInGEO.tmp
+    }
+    makeNotInGEOList ${bulkORsinglecell}_ena_gse_pooling.$today.log  > ${bulkORsinglecell}_NotInGEO_list.txt
+
+    ## remove ENA_IDs already been import before
+    filterGEOImport() {
+        dbConnection=$1
+        GSEImport=$2
+        type=$(echo $GSEImport | awk -F'_' '{print $2}')
+        if [ "$type" == "bulk" ]; then
+            GSELoaded=$(echo "select geo_acc from rnaseq_atlas_eligibility;" | psql $dbConnection | tail -n +3 | head -n -2 | sed 's/ //g' | tr '\t' '\n' | sort -u)
+        elif [ "$type" == "singlecell" ]; then 
+            GSELoaded=$(echo "select geo_acc from sc_atlas_eligibility;" | psql $dbConnection | tail -n +3 | head -n -2 | sed 's/ //g' | tr '\t' '\n' | sort -u)
+        fi
+        comm -23 <(cat $GSEImport | cut -f 1 | sort ) <(echo -e $GSELoaded | tr ' ' '\n' | sort )
+    }
+    ## filter GSE ids that have already been imported in the atlas eligibility database
+    filterGEOImport $dbConnection geo_${bulkORsinglecell}_rnaseq.tsv > latest_${bulkORsinglecell}_geo_accessions.txt
+
+    ## Download GEO import soft files and convert to MAGE-TAB format (IDF and SDRF)
+    bsub -q production-rh7 -cwd `pwd` -M 80000 -R "rusage[mem=80000]" -o ${bulkORsinglecell}_geo_import_$today.out -e ${bulkORsinglecell}_geo_import_$today.err "$projectRoot/geo_import/import_geo_subs.pl -x -f latest_${bulkORsinglecell}_geo_accessions -o $outputPath"
+    if [ $? -ne 0 ]; then
+        "ERROR: import_geo_subs.pl LSF submission for $bulkORsinglecell"  >&2  
+        exit 1
+    fi 
+popd    
+
+
+### 
---
+++ bin/geo_import_routines.sh
@@ -0,0 +1,237 @@
+#!/usr/bin/env bash
+
+geo_experiment() {
+  expAcc=$1
+  gse=$(echo "$expAcc" | sed 's/[0-9]*//g')
+  if [ "$gse" == "GSE" ]; then
+        return 0
+    else
+        return 1
+  fi
+}
+
+get_ena_id(){
+ expAcc=$1
+ geoEnaMappingFile=$2
+ idx=$(grep -n "$expAcc" "$geoEnaMappingFile" | head -n1 | awk -F":" '{print $1}')
+ awk 'FNR == '${idx}' {print $2}' "$geoEnaMappingFile"
+}
+
+elibility_error_code(){
+expAcc=$1
+pathToDownloads=$2
+
+geo_experiment "$expAcc"
+res=$(geo_experiment "$expAcc")
+  if [ $? -ne 0 ]; then
+       echo "ERROR: Not GEO experiment type - $expAcc" >&2
+  fi
+  
+  if [ -e "$pathToDownloads/${expAcc}_output/${expAcc}_atlas_eligibility.out" ]; then
+    cat "$pathToDownloads/${expAcc}_output/${expAcc}_atlas_eligibility.out" | grep "Atlas eligibility fail codes:" | awk -F":" '{print $2}' | sed 's/-//g'
+  fi
+}
+
+geo_fixable(){
+  expAcc=$1
+  pathToDownload=$2
+
+  error_code=$(elibility_error_code "$expAcc" "$pathToDownloads")
+  ERROR_CODES=$ATLAS_PROD/GEO_import/geo_import_supporting_files/error_codes.txt
+  code=$(echo "$error_code" | sed 's/,/\t/g' | cut -f 1 | sed -e 's/^\s*//')
+  comment=$(cat "$ERROR_CODES" | grep -w "$code" | awk -F"\t" '{print $3}')
+  echo -e "$comment"
+}
+
+exp_meta_info(){
+  expAcc=$1
+  pathToDownloads=$2
+
+  GEO_IMPORT_FOLDER=$(echo $pathToDownloads/${expAcc}_output)
+
+    if [[ -s "$GEO_IMPORT_FOLDER/${expAcc}_family.soft" ]]; then
+        title=$(cat "$GEO_IMPORT_FOLDER/${expAcc}_family.soft" | grep "Series_title" | awk -F"=" '{print $2}')
+        organism=$(cat "$GEO_IMPORT_FOLDER/${expAcc}_family.soft" | grep -P "Platform_organism" | awk -F"=" '{print $2}' | sort -u)
+        no_of_samples=$(cat "$GEO_IMPORT_FOLDER/${expAcc}_family.soft" | grep -c "Series_sample_id")
+   else
+    echo "ERROR: soft file doesn't exist - $expAcc"
+  fi
+  
+  if [[ -s "$GEO_IMPORT_FOLDER/${expAcc}-sdrf.txt" ]]; then
+      no_of_replicates=$(cat "$GEO_IMPORT_FOLDER/${expAcc}-sdrf.txt" | cut -d$'\t' -f 1 | tail -n+2 |  uniq -c | awk -F" " '{print $1}' | sort -u | tr "\n" " ")
+      factor_value=$(cat "$GEO_IMPORT_FOLDER/${expAcc}-sdrf.txt" | head -n1 | tr "\t" "\n" | grep -P "FactorValue" | awk -F" " '{print $2}' | sed -r 's/(\[|\])//g' | tr "\n" " ")
+  else
+    echo "ERROR: sdrf file doesn't exist - $expAcc"
+ fi
+
+  if [[ -s "$GEO_IMPORT_FOLDER/${expAcc}-idf.txt" ]]; then
+     exp_type=$(cat "$GEO_IMPORT_FOLDER/${expAcc}-idf.txt" | grep "Comment\[AEExperimentType\]"  |  cut -d$'\t' -f2 | tr "\n" " ")
+  else
+     echo "ERROR: idf file doesn't exist - $expAcc"
+  fi
+
+  echo -e "$title"'\t'"$organism"'\t'"$no_of_samples"'\t'"$no_of_replicates"'\t'"$factor_value"'\t'"$exp_type"
+}
+
+
+atlas_loaded_experiments(){
+  dbConnection=$1
+  type=$2
+
+  # experiment that are already loaded
+  if [ "$type" == "bulk" ]; then        
+      expLoaded=$(echo -e "select accession from experiment where type like 'RNASEQ%';" \
+      | psql "$dbConnection" | tail -n +3 | head -n -2 | grep "E-GEOD-" | sed 's/E-GEOD-/GSE/g' | sed 's/^ //g' | sort -u)
+  elif [ "$type" == "singlecell" ]; then 
+      expLoaded=$(echo -e "select accession from scxa_experiment where type like 'RNASEQ%';" \
+      | psql "$dbConnection" | tail -n +3 | head -n -2 | grep "E-GEOD-" | sed 's/E-GEOD-/GSE/g' | sed 's/^ //g' | sort -u)
+  fi
+
+  # experiments ongoing in Atlas
+  inAtlas=$(echo -e "select jobobject from atlas_jobs;" \
+    | psql "$dbConnection" | tail -n +3 | head -n -2 | grep "E-GEOD-" | sed 's/E-GEOD-/GSE/g' | sed 's/^ //g' | sed '/^$/d' | sort -u)
+
+  echo -e "$expLoaded\n$inAtlas" | sed 's/ //g'
+}
+
+# function to create experiment accession directory and move all processed files to GEOD
+move_files(){
+pathToDownloads=$1
+pathToCuration=$2
+
+pushd "$pathToDownloads"
+  for f in *; do
+    expAcc=$(basename "$f" | sed 's/_output//g' | sed 's/GSE/E-GEOD-/g')
+
+    echo "making directory $expAcc"
+    exp_dir="$pathToCuration/$expAcc"
+
+    if [ ! -d "$exp_dir" ]; then
+      mkdir -p "$exp_dir"
+    fi  
+
+    ## while copying preserve time stamps
+    rsync -ar $pathToDownloads/$f/*txt* $exp_dir/
+
+    # rename files with ArrayExpress accession prefix
+    rename_magetab_files $exp_dir
+
+  done
+popd
+}
+
+rename_magetab_files(){
+  pathToMagetabDir=$1
+      pushd "$pathToMagetabDir"
+        ## rename all files in the folder
+          for filename in *.txt*; do
+              mv "$filename" "$(echo "$filename" | sed 's/GSE/E-GEOD-/g')";
+          done
+      popd
+}
+
+sync_experiments_folder(){
+  expID=$1
+  pathToDownloads=$2
+  pathToCuration=$3
+
+## create ArrayExpress accession
+  expAcc=$(basename "$expID" | sed 's/_output//g' | sed 's/GSE/E-GEOD-/g')
+  
+  exp_dir_path="$pathToCuration/$expAcc"
+
+  if [ ! -d "exp_dir_path" ]; then
+      mkdir -p "$exp_dir_path"
+      # sync files
+      rsync -ar $pathToDownloads/$expID/*txt* $exp_dir_path/
+      
+      # rename files with ArrayExpress accession prefix
+      rename_magetab_files $exp_dir_path
+  fi    
+}
+
+exp_loading_check(){
+  expAcc=$1
+  geoEnaMappingFile=$2
+  dbConnection=$3
+  type=$4
+  pathToDownloads=$5
+
+  if [ "$type" == "bulk" ]; then        
+      count=$(echo "select count(*) from rnaseq_atlas_eligibility where geo_acc='$expAcc';" | psql "$dbConnection" | tail -n +3 | head -n1 | sed 's/ //g')
+  elif [ "$type" == "singlecell" ]; then 
+      count=$(echo "select count(*) from sc_atlas_eligibility where geo_acc='$expAcc';" | psql "$dbConnection" | tail -n +3 | head -n1 | sed 's/ //g')
+  fi
+
+  count_in_atlas=$(atlas_loaded_experiments "$dbConnection" "$type" | grep -c "$expAcc")
+
+  if [ "$count_in_atlas" -ne 0 ]; then
+        echo "$expAcc - previously loaded in $type atlas table"
+        return 1
+  elif [ "$count" -ne 0 ]; then
+        echo "$expAcc - already exist in $type atlas eligbility table"
+        return 1
+  else
+        echo "loading $expAcc in db"
+        load_eligibility_to_db "$expAcc" "$geoEnaMappingFile" "$dbConnection" "$type" "$pathToDownloads"
+        return 0
+  fi
+}
+
+load_eligibility_to_db(){
+  expAcc=$1
+  geoEnaMappingFile=$2
+  dbConnection=$3
+  type=$4 
+  pathToDownloads=$5
+
+  ena_id=$(get_ena_id "$expAcc" "$geoEnaMappingFile")
+   if [[ -z "$ena_id" ]]; then
+      echo "ENA-ID doesn't match for $expAcc" >&2
+   fi
+   atlas_id=$(echo -e "$expAcc" | sed 's/GSE/E-GEOD-/g')
+   error_code=$(elibility_error_code "$expAcc" "$pathToDownloads")
+   comment=$(geo_fixable "$expAcc" "$pathToDownloads")
+   title=$(exp_meta_info "$expAcc" "$pathToDownloads" | awk -F'\t' '{print $1}')
+   organism=$(exp_meta_info "$expAcc" "$pathToDownloads" | awk -F'\t' '{print $2}')
+   no_of_samples=$(exp_meta_info "$expAcc" "$pathToDownloads" | awk -F'\t' '{print $3}')
+   no_of_replicate=$(exp_meta_info "$expAcc" "$pathToDownloads" | awk -F'\t' '{print $4}')
+   factor_value=$(exp_meta_info "$expAcc" "$pathToDownloads" | awk -F'\t' '{print $5}')
+   exp_type=$(exp_meta_info "$expAcc" "$pathToDownloads" | awk -F'\t' '{print $6}')
+
+  if [ $type == "bulk" ]; then
+       echo "Loading $expAcc to rnaseq_atlas_eligibility"    
+       echo "insert into rnaseq_atlas_eligibility values (current_timestamp(0),'$atlas_id','$ena_id','$expAcc','$error_code', '$comment','$title','$organism','$no_of_samples','$no_of_replicate','$factor_value',current_timestamp(0),NULL,NULL,'$exp_type');" | psql "$dbConnection"
+
+  elif [ $type == "singlecell" ]; then 
+        echo "Loading $expAcc to sc_atlas_eligibility"    
+        echo "insert into sc_atlas_eligibility values (current_timestamp(0),'$atlas_id','$ena_id','$expAcc','$error_code','$comment','$title','$organism','$no_of_samples','$no_of_replicate','$factor_value',current_timestamp(0),NULL,NULL,'$exp_type');" | psql "$dbConnection"
+  fi
+}
+
+
+# query experiments for species that are having reference genome in ISL
+# extract species and subspecies from RNA-seqer API call for ensembl, plants metazoa and wbps.
+query_rnaseq_atlas_eligibility() {
+IFS="
+"
+dbConnection=$1
+
+species_in_ensembl=$(curl -s "https://www.ebi.ac.uk/fg/rnaseq/api/tsv/0/getOrganisms/ensembl" | cut -f 1 | tail -n +2 | awk '{ gsub("_", " ") ; print $0 }' | sed -e 's/^./\U&/')
+species_in_plants=$(curl -s "https://www.ebi.ac.uk/fg/rnaseq/api/tsv/0/getOrganisms/plants" | cut -f 1 | tail -n +2 | awk '{ gsub("_", " ") ; print $0 }' | sed -e 's/^./\U&/')
+species_in_metazoa=$(curl -s "https://www.ebi.ac.uk/fg/rnaseq/api/tsv/0/getOrganisms/metazoa" | cut -f 1 | tail -n +2 | awk '{ gsub("_", " ") ; print $0 }' | sed -e 's/^./\U&/')
+species_in_wbps=$(curl -s "https://www.ebi.ac.uk/fg/rnaseq/api/tsv/0/getOrganisms/wbps" | cut -f 1 | tail -n +2 | awk '{ gsub("_", " ") ; print $0 }' | sed -e 's/^./\U&/')
+
+species_in_isl=$(printf '%s\n' "$species_in_ensembl" "$species_in_plants" "$species_in_metazoa" "$species_in_wbps")
+output=""
+echo -e "a2_acc'\t'na_study_id'\t'geo_acc'\t'organism'\t'status'\t'exp_type"
+for species in $species_in_isl; do
+    var=$(echo "select DISTINCT ae2_acc, ena_study_id, geo_acc, organism,status, exp_type from rnaseq_atlas_eligibility where organism like'%$species';" | psql "$dbConnection" | tail -n +3 | head -n -2)
+     if [[ -z $var ]]; then
+      echo "query didn't succeed for the $species" > /dev/null
+     fi
+     output+="$var\n"
+ done
+
+echo -e "$output" | sed -e '/^ *$/d'
+}

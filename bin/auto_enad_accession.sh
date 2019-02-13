3c1e4253ee3096b5d05ddfbd5c2f018e187f529d
Suhaib Mohammed <suhaib58@gmail.com>
2019-02-13 10:34:53 +0000

    Feature/auto ena path (#7)
    * source routines path fix
    
    * script dir to navigate to script

--- bin/auto_enad_accession.sh
+++ bin/auto_enad_accession.sh
@@ -2,7 +2,7 @@
 
 scriptDir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 projectRoot=${scriptDir}/..
-source $projectRoot/bash_util/generic_routines.sh
+source $projectRoot/../bash_util/generic_routines.sh
 
 # Capture call arguments
 if [ $# -lt 2 ]; then
@@ -16,7 +16,7 @@ dbIdentifier=$2
 
 dbConnection=$(get_db_connection $pgAtlasUser $dbIdentifier)
 
-max_db_id=$(psql -d "$dbConnection" -a -f auto_enad_accession.sql | grep "E-ENAD-" | tail -n1 | sed 's/[^0-9]*//g')
+max_db_id=$(psql -d "$dbConnection" -a -f $scriptDir/auto_enad_accession.sql | grep "E-ENAD-" | tail -n1 | sed 's/[^0-9]*//g')
 
 max_single_id=$(find $ATLAS_PROD/singlecell/experiment -type f -name "E-ENAD-*" -exec basename {} ';' | sed 's/[^0-9]*//g' | sort -nr | head -n1)
 
